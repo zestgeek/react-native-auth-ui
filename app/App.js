b14c03f50ca19168356f71c09d69f6f960dffa27
Ram Kumar <ramkumar897003@gmail.com>
2020-04-01 21:58:11 +0200

    Login screen UI

--- app/App.js
+++ app/App.js
@@ -1,108 +1,50 @@
-import React from "react";
-import { StyleSheet, Text, View, AppRegistry } from "react-native";
+import React from 'react';
+import {
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import {NativeRouter} from 'react-router-native';
 
-import { NativeRouter, Route, Link } from "react-router-native";
-
-const Home = () => <Text style={styles.header}>Home</Text>;
-
-const About = () => <Text style={styles.header}>About</Text>;
-
-const Topic = ({ match }) => (
-  <Text style={styles.topic}>{match.params.topicId}</Text>
-);
-
-const Topics = ({ match }) => (
-  <View>
-    <Text style={styles.header}>Topics</Text>
-    <View>
-      <Link
-        to={`${match.url}/rendering`}
-        style={styles.subNavItem}
-        underlayColor="#f0f4f7"
-      >
-        <Text>Rendering with React</Text>
-      </Link>
-      <Link
-        to={`${match.url}/components`}
-        style={styles.subNavItem}
-        underlayColor="#f0f4f7"
-      >
-        <Text>Components</Text>
-      </Link>
-      <Link
-        to={`${match.url}/props-v-state`}
-        style={styles.subNavItem}
-        underlayColor="#f0f4f7"
-      >
-        <Text>Props v. State</Text>
-      </Link>
-    </View>
-
-    <Route path={`${match.path}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.path}
-      render={() => (
-        <Text style={styles.topic}>Please select a topic.</Text>
-      )}
-    />
-  </View>
-);
+import Login from './Containers/Login';
 
 const App = () => (
   <NativeRouter>
-    <View style={styles.container}>
-      <View style={styles.nav}>
-        <Link to="/" underlayColor="#f0f4f7" style={styles.navItem}>
-          <Text>Home</Text>
-        </Link>
-        <Link
-          to="/about"
-          underlayColor="#f0f4f7"
-          style={styles.navItem}
-        >
-          <Text>About</Text>
-        </Link>
-        <Link
-          to="/topics"
-          underlayColor="#f0f4f7"
-          style={styles.navItem}
-        >
-          <Text>Topics</Text>
-        </Link>
-      </View>
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <SafeAreaView style={{flex: 1}}>
+        <StatusBar barStyle="dark-content" />
+        <Login />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   </NativeRouter>
 );
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 25,
-    padding: 10
+    padding: 10,
   },
   header: {
-    fontSize: 20
+    fontSize: 20,
   },
   nav: {
-    flexDirection: "row",
-    justifyContent: "space-around"
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
   navItem: {
     flex: 1,
-    alignItems: "center",
-    padding: 10
+    alignItems: 'center',
+    padding: 10,
   },
   subNavItem: {
-    padding: 5
+    padding: 5,
   },
   topic: {
-    textAlign: "center",
-    fontSize: 15
-  }
+    textAlign: 'center',
+    fontSize: 15,
+  },
 });
 
-export default App
+export default App;
---
+++ app/Containers/Login.js
@@ -0,0 +1,138 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {Link, Route} from 'react-router-native';
+
+import ForgotPassword from './ForgotPassword';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.headerTitle}>
+          <Text style={styles.headerText}>Log in</Text>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <View style={[styles.inputWrapper, styles.firstInput]}>
+              <TextInput
+                placeholder="Email"
+                placeholderTextColor="#696969"
+                style={styles.textInput}
+              />
+              <Image
+                source={require('../assets/images/mail/mail.png')}
+                style={styles.icon}
+              />
+            </View>
+            <View style={styles.inputWrapper}>
+              <TextInput
+                placeholder="Password"
+                placeholderTextColor="#696969"
+                style={styles.textInput}
+              />
+              <Image
+                source={require('../assets/images/password/password.png')}
+                style={styles.icon}
+              />
+            </View>
+            <Link
+              to="/"
+              underlayColor="#f0f4f7"
+              style={styles.navItemContainer}>
+              <Text style={styles.navItemText}>Forgot password ?</Text>
+            </Link>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Log in</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <Route exact path="/forgotpassword" component={ForgotPassword} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8CC33F',
+  },
+  headerTitle: {
+    marginTop: 25,
+    padding: 20,
+    flex: 1,
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontFamily: 'Poppins-Semibold',
+    letterSpacing: 0,
+  },
+  content: {
+    backgroundColor: '#fff',
+    flex: 3,
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+  },
+  form: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 72,
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    flex: 1,
+    color: '#696969',
+    padding: 20,
+    paddingLeft: 0,
+    borderBottomWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#707070',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 16,
+  },
+  firstInput: {
+    marginBottom: 45,
+  },
+  icon: {
+    flex: 1,
+    resizeMode: 'stretch',
+    position: 'absolute',
+    right: 0,
+  },
+  navItemContainer: {
+    marginTop: 35,
+    marginBottom: 35,
+    alignSelf: 'center',
+  },
+  navItemText: {
+    fontSize: 18,
+    color: '#696969',
+    fontFamily: 'Poppins-Medium',
+  },
+  button: {
+    backgroundColor: '#8CC33F',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    alignSelf: 'center',
+    borderRadius: 30,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontFamily: 'Poppins-Medium',
+  },
+});
