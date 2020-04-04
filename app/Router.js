91ab6443ea55248223e2175d1c1bab4725444950
Ram Kumar <ramkumar897003@gmail.com>
2020-04-04 20:49:45 +0200

    Integrated settings page

--- app/Router.js
+++ app/Router.js
@@ -8,15 +8,17 @@ import Signup from './Containers/Signup';
 import ResetPassword from './Containers/ResetPassword';
 import ForgotPassword from './Containers/ForgotPassword';
 import Account from './Containers/Account';
+import Settings from './Containers/Settings';
 
 const Routes = () => (
   <AppLayout>
     <NativeRouter>
-      <Route exact path="/" component={Account} />
+      <Route exact path="/" component={Settings} />
       <Route path="/register" component={Signup} />
       <Route path="/resetpassword" component={ResetPassword} />
       <Route path="/forgotpassword" component={ForgotPassword} />
       <Route path="/account" component={Account} />
+      <Route path="/settings" component={Settings} />
     </NativeRouter>
   </AppLayout>
 );
